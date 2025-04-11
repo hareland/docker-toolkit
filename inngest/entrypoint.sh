ce3050366b189ccc5bf8b7ea2636f93c6c490cea
hareland <mrhareland+packagist@gmail.com>
2025-04-11 18:33:04 +0200

    feat: add optional config file in entrypoint.sh

--- inngest/entrypoint.sh
+++ inngest/entrypoint.sh
@@ -8,5 +8,12 @@ for key in "${KEYS[@]}"; do
   EVENT_KEYS="$EVENT_KEYS --event-key $key"
 done
 
+# If a config file is provided, we want to load it...
+CONFIG_OPTION=""
+if [[ -n $INNGEST_CONFIG_FILE ]]; then
+  CONFIG_OPTION=" --config $INNGEST_CONFIG_FILE"
+fi
+
+
 # Start the Inngest server with the appropriate flags
-exec inngest start --config /app/config/inngest.yml "$EVENT_KEYS" --signing-key "$INNGEST_SIGNING_KEY"
+exec inngest start"$CONFIG_OPTION" "$EVENT_KEYS" --signing-key "$INNGEST_SIGNING_KEY"
